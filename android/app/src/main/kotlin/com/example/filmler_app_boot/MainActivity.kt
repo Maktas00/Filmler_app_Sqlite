2d3cacac43778b953ad1d38193134cb371ec27b0
Mertay Aktaş <mertayaktas@hotmail.com>
2024-03-10 01:12:13 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/filmler_app_boot/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.filmler_app_boot
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/data/repo/filmlerdao_repository.dart
@@ -0,0 +1,15 @@
+import 'package:filmler_app_boot/data/entity/filmler.dart';
+import 'package:filmler_app_boot/sqlite/veritabani_yardimcisi.dart';
+
+class FilmlerdaoRepository{
+  Future<List<Filmler>> filmleriYukle() async{
+    var db=await VeritabaniYardimcisi.veritabaniErisim();
+    List<Map<String,dynamic>> maps=await db.rawQuery("SELECT*FROM filmler");
+    
+    return List.generate(maps.length, (i) {
+      var satir=maps[i];
+      return Filmler(id: satir["id"], ad: satir["ad"], resim: satir["resim"], fiyat: satir["fiyat"]);
+
+    });
+  }
+}
